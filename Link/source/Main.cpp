965eceee6d630b1892f2d6dcd11aee1f1c21eb6a
Eric Laukien <eric@rocketcitystudios.com>
2015-10-18 00:45:25 -0400

    Fixed cmakelists typo

--- Link/source/Main.cpp
+++ Link/source/Main.cpp
@@ -22,20 +22,34 @@ int main() {
 
 	std::mt19937 generator(time(nullptr));
 
+	std::string test = "This is a string to test the prediction capabilities of PredictiveRSDR.";
+
+	int minimum = 255;
+	int maximum = 0;
+
+	for (int i = 0; i < test.length(); i++) {
+		minimum = std::min(static_cast<int>(test[i]), minimum);
+		maximum = std::max(static_cast<int>(test[i]), maximum);
+	}
+
+	int numInputs = maximum - minimum;
+
+	int inputsRoot = std::ceil(std::sqrt(static_cast<float>(numInputs)));
+
 	std::vector<sdr::PredictiveRSDR::LayerDesc> layerDescs(3);
 
-	layerDescs[0]._width = 16;
-	layerDescs[0]._height = 16;
+	layerDescs[0]._width = 32;
+	layerDescs[0]._height = 32;
 
-	layerDescs[1]._width = 8;
-	layerDescs[1]._height = 8;
+	layerDescs[1]._width = 24;
+	layerDescs[1]._height = 24;
 
-	layerDescs[2]._width = 4;
-	layerDescs[2]._height = 4;
+	layerDescs[2]._width = 16;
+	layerDescs[2]._height = 16;
 
 	sdr::PredictiveRSDR prsdr;
 
-	prsdr.createRandom(1, 1, layerDescs, -0.01f, 0.01f, 0.01f, 1.0f, generator);
+	prsdr.createRandom(inputsRoot, inputsRoot, layerDescs, -0.01f, 0.01f, 0.01f, 1.0f, generator);
 
 	// ---------------------------- Game Loop -----------------------------
 
@@ -47,7 +61,7 @@ int main() {
 
 	float dt = 0.017f;
 
-	float x = 0.0f;
+	int current = 0;
 
 	do {
 		clock.restart();
@@ -71,15 +85,28 @@ int main() {
 
 		window.clear();
 
-		prsdr.setInput(0, std::sin(x));
+		for (int i = 0; i < numInputs; i++)
+			prsdr.setInput(i, 0.0f);
+
+		int index = test[current] - minimum;
+
+		prsdr.setInput(index, 1.0f);
 
 		prsdr.simStep();
 
-		std::cout << std::sin(x) << " " << prsdr.getPrediction(0) << std::endl;
+		int predIndex = 0;
 
-		x += 0.5f;
+		for (int i = 1; i < numInputs; i++)
+			if (prsdr.getPrediction(i) > prsdr.getPrediction(predIndex))
+				predIndex = i;
+
+		char predChar = predIndex + minimum;
+
+		std::cout << predChar;
+
+		current = (current + 1) % test.length();
 		
-		window.display();
+		//window.display();
 	} while (!quit);
 
 	return 0;
