157c186d431c33a4bfd476b82ae71c5f8980319c
Eric Laukien <eric@rocketcitystudios.com>
2015-10-26 22:36:04 -0400

    Different inhibition scheme

--- Link/source/Main.cpp
+++ Link/source/Main.cpp
@@ -40,11 +40,11 @@ int main() {
 
 	std::vector<sdr::PredictiveRSDR::LayerDesc> layerDescs(3);
 
-	layerDescs[0]._width = 16;
-	layerDescs[0]._height = 16;
+	layerDescs[0]._width = 32;
+	layerDescs[0]._height = 32;
 
-	layerDescs[1]._width = 12;
-	layerDescs[1]._height = 12;
+	layerDescs[1]._width = 16;
+	layerDescs[1]._height = 16;
 
 	layerDescs[2]._width = 8;
 	layerDescs[2]._height = 8;
